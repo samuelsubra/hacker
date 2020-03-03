2c41ffd6db8302c3252784c059ae670ae87f3329
samuelsubra <60764394+samuelsubra@users.noreply.github.com>
2020-03-02 19:56:32 -0500

    Add files via upload

---
+++ Hacker v1.py
@@ -0,0 +1,35 @@
+# Hacker
+# Version 1
+
+# display header
+print('DEBUG MODE')
+print('1 ATTEMPT(S) LEFT')
+print('')
+
+# display passwords
+print('PROVIDE')
+print('SETTING')
+print('CANTINA')
+print('CUTTING')
+print('HUNTERS')
+print('SURVIVE')
+print('HEARING')
+print('HUNTING')
+print('REALIZE')
+print('NOTHING')
+print('OVERLAP')
+print('FINDING')
+print('PUTTING')
+print('')
+
+# prompt user
+guess = input('Enter password >')
+
+# display outcome
+print(guess)
+print('')
+print('LOGIN FAILURE - TERMINAL LOCKED')
+print('')
+print('PLEASE CONTACT AN ADMINISTRATOR')
+print('')
+input('PRESS ENTER TO EXIT')
